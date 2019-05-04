6ebbc345c4c0bdd12f76843d8e4e412927ff0110
MeSandwich <krlichc@yahoo.com>
2019-05-04 01:39:23 -0400

    I give up

--- LAG_Project/src/com/DFAGenerator.java
+++ LAG_Project/src/com/DFAGenerator.java
@@ -15,30 +15,29 @@ class DFAGenerator
     }
 
 
-    DFA create()
-    {
+    DFA create() {
         DFA outDFA = new DFA();
 
         transitions = nfaToTranslate.getTransitionsAsStrings();
 
 
         //Fetches all the transition types that aren't E
-        for(String i : transitions)   //fetch the transitions
+        for (String i : transitions)   //fetch the transitions
         {
             String[] splitParts = i.split(","); //Split the transition up
             int fromState = Integer.parseInt("" + splitParts[0].charAt(1)); //Get the from state
             int toState = Integer.parseInt("" + splitParts[1].charAt(0));   //get the to state
 
             //add all possible states
-            if(!states.contains(fromState))
+            if (!states.contains(fromState))
                 states.add(fromState);
-            if(!states.contains(toState))
+            if (!states.contains(toState))
                 states.add(toState);
 
             //get the transition char
             char transition = splitParts[2].charAt(0);
 
-            if(transition != 'E')  //add all transitions that aren't E
+            if (transition != 'E')  //add all transitions that aren't E
             {
                 transitionTypes.add(transition);
             }
@@ -52,15 +51,14 @@ class DFAGenerator
         //needs to be strings
         String[][] eStarTable = new String[eStarXSize][eStarYSize];
 
-        for(String t : transitions) {
+        for (String t : transitions) {
             String[] splitParts = t.split(","); //Split the transition up
             int fromState = Integer.parseInt("" + splitParts[0].charAt(1)); //Get the from state
             int toState = Integer.parseInt("" + splitParts[1].charAt(0));   //get the to state
             char transition = splitParts[2].charAt(0);
 
             //fill out non E move in table first
-            if (transition != 'E')
-            {
+            if (transition != 'E') {
                 int xIndex = transitionTypes.indexOf(transition);
                 int yIndex = states.indexOf(fromState);
 
@@ -70,28 +68,57 @@ class DFAGenerator
 
 
         //Add the basic loop back E transition
-        for(int i = 0; i < eStarYSize; i++)
-        {
+        for (int i = 0; i < eStarYSize; i++) {
             eStarTable[eStarXSize - 1][i] = String.valueOf(states.get(i));
         }
 
         //find all E* transitions
-        for(String t : transitions)
-        {
+        for (String t : transitions) {
+            String[] splitParts = t.split(","); //Split the transition up
+            int fromState = Integer.parseInt("" + splitParts[0].charAt(1)); //Get the from state
+            int toState = Integer.parseInt("" + splitParts[1].charAt(0));   //get the to state
+            char transition = splitParts[2].charAt(0);
 
+            if (transition == 'E') {
+                int yIndex = states.indexOf(fromState);
+                eStarTable[eStarXSize - 1][yIndex] += "," + String.valueOf(toState);
+            }
         }
 
-        System.out.println(transitionTypes.toString());
-        System.out.println(states.toString());
+        String[][] dfaFinalTable = new String[eStarXSize][1000];    //big enough table for dynamic allocations
+        boolean newRowAddedFlag = true;
 
+        //creates the final dfa table
+        for (int i = 0; i < eStarXSize; i++) {
+            for (int j = 0; j < 1000; j++)
+            {
+                if(!newRowAddedFlag)
+                    break;
+                else
+                {
+                    //if it is not a null entry
+                    if(eStarTable[i][j] != "-" && eStarTable[i][j] != null)
+                    {
+                        int yIndex = Integer.parseInt(eStarTable[i][j]);    //get the index for the table result
+
+                        String rowResult = eStarTable[eStarXSize - 1][yIndex];
+                        System.out.println(rowResult);
+                    }
+                }
+            }
+        }
+
+
+
+        //For testing purposes
         for (int i = 0; i < eStarXSize; i++)
         {
             for(int j = 0; j < eStarYSize; j++)
             {
                 if(eStarTable[i][j] != null)
-                    System.out.print(eStarTable[i][j]);
+                    System.out.print(eStarTable[i][j] + " ");
                 else
-                    System.out.print('-');
+                    System.out.print('-' + " ");
             }
             System.out.println();
         }
